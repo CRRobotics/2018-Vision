023e04af300a0bc8a42ea773cf0107753fac5257
ICSD\james.eginton <ICSD\james.eginton@icsd.k12.ny.us>
2018-01-30 18:35:13 -0500

    asdfasdfasdfasdfdasf

---
+++ src/Distance.java
@@ -0,0 +1,15 @@
+
+public class Distance {
+
+    //Variable
+    private static double FOV = 60;
+    //distance from object
+
+    public double getFOV(){return FOV;}
+    public void setFOV(double newFOV){FOV = newFOV;}
+
+    public static double getDistance(double knownLength, int measuredPix, int maxPix) {
+        return (Math.sin(90+measuredPix*FOV/maxPix)*knownLength/2)/Math.sin(measuredPix*FOV/maxPix);
+    }
+
+}
