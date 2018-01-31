5063ce1eaefbf89aaa2c606bca7534846f827be8
roary richardson <roary.richardson@icsd.k12.ny.us>
2018-01-31 17:58:29 -0500

    fixed the errors from copying over the equation

--- src/Distance.java
+++ src/Distance.java
@@ -9,7 +9,7 @@ public class Distance {
     public void setFOV(double newFOV){FOV = newFOV;}
 
     public static double getDistance(double knownLength, int measuredPix, int maxPix) {
-        return (Math.sin(90+measuredPix*FOV/maxPix)*knownLength/2)/Math.sin(measuredPix*FOV/maxPix);
+        return (Math.sin(90-((measuredPix/2)*(FOV/maxPix)))*knownLength/2)/Math.sin((measuredPix/2)*(FOV/maxPix));
     }
 
 }
