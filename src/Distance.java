f177e7c62fa789f3bcc92fe35bdbc95e4fb1e7a7
roary richardson <roary.richardson@icsd.k12.ny.us>
2018-02-01 17:48:50 -0500

    added FOV as a parameter

--- src/Distance.java
+++ src/Distance.java
@@ -1,15 +1,6 @@
 
 public class Distance {
-
-    //Variable
-    private static double FOV = 60;
-    //distance from object
-
-    public double getFOV(){return FOV;}
-    public void setFOV(double newFOV){FOV = newFOV;}
-
-    public static double getDistance(double knownLength, int measuredPix, int maxPix) {
+    public static double getDistance(double knownLength, int measuredPix, int maxPix, double FOV) {
         return (Math.sin(90-((measuredPix/2)*(FOV/maxPix)))*knownLength/2)/Math.sin((measuredPix/2)*(FOV/maxPix));
     }
-
 }
