b7651b6aa6c5565cd8c72b25d8d2d7789af47999
ICSD\roary.richardson <roary.richardson@icsd.k12.ny.us>
2018-02-08 19:02:19 -0500

    added functionality for 2 strips

--- PaulVision/src/Constants.java
+++ PaulVision/src/Constants.java
@@ -1,5 +1,5 @@
 public class Constants {
-    public static final double KNOWNLENGTH = 9.5;
+    public static final double KNOWNLENGTH = 15.5;
     public static final int FL = 800;
     public static final double FOV = 60.0;
     public static final int MAXPIX = 640;
--- PaulVision/src/Main.java
+++ PaulVision/src/Main.java
@@ -1,6 +1,5 @@
 import javafx.geometry.BoundingBox;
 import org.opencv.core.*;
-import org.opencv.core.Point;
 import org.opencv.videoio.VideoCapture;
 //import org.opencv.highgui.ImageWindow;
 
@@ -9,6 +8,7 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import org.opencv.imgproc.*;
+
 
 
 public class Main {
@@ -41,7 +41,6 @@ public class Main {
 
 
         Mat frame = new Mat();
-        Rect rect2 = null;
         Mat f2 = null;
         camera.read(frame);
         //if(camera.read() = true) System.out.println();
@@ -59,17 +58,16 @@ public class Main {
                 if (camera.read(frame)) {
                     f2 = Mat.zeros(frame.rows(), frame.cols(), CvType.CV_8UC3);
                     cog.process(frame);
-                    Rect rect = null;
+                    RotatedRect[] rects;
                     List<MatOfPoint> convexHulls = cog.convexHullsOutput();
                     for(MatOfPoint hull : convexHulls) {
                         MatOfPoint2f hp = new MatOfPoint2f();
                         hull.convertTo(hp, CvType.CV_32F);
                         MatOfPoint2f new_curve = new MatOfPoint2f();
-                        Imgproc.approxPolyDP(hp, new_curve, Imgproc.arcLength(hp, true) * .01, true);
+                        Imgproc.approxPolyDP(hp, new_curve, Imgproc.arcLength(hp, true) * .005, true);
                         MatOfPoint int_new_curve = new MatOfPoint();
                         new_curve.convertTo(int_new_curve, CvType.CV_32S);
                         Imgproc.drawContours(f2, Arrays.asList(int_new_curve), 0, new Scalar(0, 255, 0));
-                        rect = Imgproc.boundingRect(int_new_curve);
                     }
                     /**
                      * roary's attempt to print only the pixel length of the longest line
@@ -82,6 +80,47 @@ public class Main {
 
                     Point[] ap;
                     double longest = 0;
+                    MatOfPoint2f         approxCurve = new MatOfPoint2f();
+                    rects = new RotatedRect[contours.size()];
+                    //For each contour found
+                    for (int i=0; i<contours.size(); i++)
+                    {
+                        //Convert contours(i) from MatOfPoint to MatOfPoint2f
+                        MatOfPoint2f contour2f = new MatOfPoint2f( contours.get(i).toArray() );
+                        //Processing on mMOP2f1 which is in type MatOfPoint2f
+                        double approxDistance = Imgproc.arcLength(contour2f, true)*0.005;
+                        Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
+
+                        //Convert back to MatOfPoint
+                        MatOfPoint points = new MatOfPoint( approxCurve.toArray() );
+
+                        // Get bounding rect of contour
+                        rects[i] = Imgproc.minAreaRect(approxCurve);
+                        Point[] pts = new Point[4];
+                        rects[i].points(pts);
+
+                        // draw enclosing rectangle (all same color, but you could use variable i to make them unique)
+                        for(int j=0; j<pts.length; ++j){
+                            Imgproc.line(frame, pts[j], pts[(j+1)%pts.length], new Scalar(0,255,0));
+                        }
+                    }
+                    if(rects != null && rects.length > 2) {
+                        RotatedRect rect1 = rects[0];
+                        RotatedRect rect2 = rects[1];
+                        for (RotatedRect r : rects) {
+                            if (r.size.height + r.size.width > rect1.size.height + rect1.size.width)
+                                rect1 = r;
+                        }
+                        for (RotatedRect r2 : rects) {
+                            if (r2.size.height + r2.size.width > rect2.size.height + rect2.size.width && !r2.equals(rect1))
+                                rect1 = r2;
+                        }
+//                        System.out.println(QuickMath.getAngle((int)((rect1.center.x + rect2.center.x)/2 + .5)));
+                        if(rect1.size.height > rect1.size.width)
+                            System.out.println(QuickMath.getDistance((int)(rect1.size.height + .5)));
+                        else
+                            System.out.println(QuickMath.getDistance((int)(rect1.size.width + .5)));
+                    }
 
 //                    for(MatOfPoint cont : contours) {
 //                        mop2f = new MatOfPoint2f();
@@ -97,12 +136,12 @@ public class Main {
 ////                            longest = current;
 ////                        System.out.println(ap.length);
 //                    }
-                    if(rect != null) {
-                        Imgproc.rectangle(frame, rect.tl(), rect.br(), new Scalar(0, 255, 0), 2);
+//                    if(rect != null) {
+//                        Imgproc.rectangle(frame, rect.tl(), rect.br(), new Scalar(0, 255, 0), 2);
 ////                        System.out.println(QuickMath.getAngle(rect.x + rect.width/2));
 ////                        System.out.println(QuickMath.getDistance(rect.width));
 ////                        System.out.println(longest);
-                    }
+//                    }
 
                     window.image(frame);
                     window2.image(f2);
